8adf6b67913a6a33fbebe9527b30c8431aa1bea9
1dontknowwhatyouwant <milestone0917@gmail.com>
2024-06-25 14:46:21 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 184 ms, Memory: 108080 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+if n == n:
+    print(n//n)
+    print(0)
+elif n==(n*n):
+    print(n//(n*n))
+    print(2)
+elif n==(n*n*n):
+    print(n//(n*n*n))
+    print(3)
+
+else:
+    print(4)
