0c8c516b3746d69f7a34c400e9c5826c9b35cf5f
1dontknowwhatyouwant <milestone0917@gmail.com>
2024-06-26 20:56:58 +0900

    [Silver III] Title: 모두의 마블, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/12845. 모두의 마블/모두의 마블.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+L = list(map(int, input().split()))
+
+L.sort(reverse=True)
+
+gold = 0
+
+for i in range(1, n):
+    gold += L[0] + L[i]
+
+print(gold)
