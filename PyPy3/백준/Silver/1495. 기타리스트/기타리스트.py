feb58b5fbae9fdf4f58e89229f1ef21d2f8cf460
1dontknowwhatyouwant <milestone0917@gmail.com>
2024-07-02 22:53:22 +0900

    [Silver I] Title: 기타리스트, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/1495. 기타리스트/기타리스트.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N,S,M = map(int, input().split())
+volumes = list(map(int, input().split()))
+
+dp = [[False] * (M + 1) for _ in range(N + 1)]
+dp[0][S] = True
+
+for i in range(1, N + 1):
+    for j in range(M + 1):
+        if dp[i-1][j]:
+            if j + volumes[i-1] <= M: 
+                dp[i][j + volumes[i-1]] = True
+            if j - volumes[i-1] >= 0: 
+                dp[i][j - volumes[i-1]] = True
+
+result = -1
+for i in range(M, -1, -1):
+    if dp[N][i]:
+        result = i
+        break
+
+print(result)
