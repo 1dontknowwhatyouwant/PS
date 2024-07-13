22aff381c9ee3697c0460e45ce428d89c2e12cbe
1dontknowwhatyouwant <milestone0917@gmail.com>
2024-07-13 23:23:49 +0900

    Update 월드컵.py

--- PyPy3/백준/Gold/6987. 월드컵/월드컵.py
+++ PyPy3/백준/Gold/6987. 월드컵/월드컵.py
@@ -1,39 +1,41 @@
-def is_possible(index, wins, draws, losses):
+def dfs(index):
     if index == 15:
-        return all(w == 0 for w in wins) and all(d == 0 for d in draws) and all(l == 0 for l in losses)
+        # 모든 경기를 다 시뮬레이션했으면 주어진 결과와 일치하는지 확인
+        for i in range(6):
+            if wins[i] != win_temp[i] or draws[i] != draw_temp[i] or losses[i] != loss_temp[i]:
+                return False
+        return True
     
     team1, team2 = matches[index]
     
     # team1이 이기고, team2가 지는 경우
-    if wins[team1] > 0 and losses[team2] > 0:
-        wins[team1] -= 1
-        losses[team2] -= 1
-        if is_possible(index + 1, wins, draws, losses):
-            return True
-        wins[team1] += 1
-        losses[team2] += 1
+    win_temp[team1] += 1
+    loss_temp[team2] += 1
+    if dfs(index + 1):
+        return True
+    win_temp[team1] -= 1
+    loss_temp[team2] -= 1
     
     # team1과 team2가 비기는 경우
-    if draws[team1] > 0 and draws[team2] > 0:
-        draws[team1] -= 1
-        draws[team2] -= 1
-        if is_possible(index + 1, wins, draws, losses):
-            return True
-        draws[team1] += 1
-        draws[team2] += 1
+    draw_temp[team1] += 1
+    draw_temp[team2] += 1
+    if dfs(index + 1):
+        return True
+    draw_temp[team1] -= 1
+    draw_temp[team2] -= 1
     
     # team1이 지고, team2가 이기는 경우
-    if losses[team1] > 0 and wins[team2] > 0:
-        losses[team1] -= 1
-        wins[team2] -= 1
-        if is_possible(index + 1, wins, draws, losses):
-            return True
-        losses[team1] += 1
-        wins[team2] += 1
+    loss_temp[team1] += 1
+    win_temp[team2] += 1
+    if dfs(index + 1):
+        return True
+    loss_temp[team1] -= 1
+    win_temp[team2] -= 1
     
     return False
 
 def solve():
+    global wins, draws, losses, win_temp, draw_temp, loss_temp
     results = []
     for _ in range(4):
         data = list(map(int, input().split()))
@@ -45,7 +47,11 @@ def solve():
             results.append(0)
             continue
         
-        if is_possible(0, wins, draws, losses):
+        win_temp = [0] * 6
+        draw_temp = [0] * 6
+        loss_temp = [0] * 6
+        
+        if dfs(0):
             results.append(1)
         else:
             results.append(0)
@@ -53,5 +59,6 @@ def solve():
     print(' '.join(map(str, results)))
 
 matches = [(i, j) for i in range(6) for j in range(i + 1, 6)]
+
 
 solve()
